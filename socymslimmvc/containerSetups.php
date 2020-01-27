ea3ec241ff1aa433796255e7607c1a8324ac4325
Shinzo SAITO <architshin@websarva.com>
2020-01-27 12:02:00 +0900

    12章のスケルトンプロジェクトの追加。

---
+++ socymslimmvc/containerSetups.php
@@ -0,0 +1,12 @@
+<?php
+use DI\Container;
+use Slim\Factory\AppFactory;
+use Slim\Views\Twig;
+$container = new Container();
+$container->set("view",
+	function() {
+		$twig = Twig::create($_SERVER["DOCUMENT_ROOT"]."/../templates");
+		return $twig;
+	}
+);
+AppFactory::setContainer($container);
